71db7bc657cb15bc11e3e800bfa389f0c5b1dbdd
Charlie Egan <charlieegan3@users.noreply.github.com>
2017-08-27 23:57:32 +0100

    Implement handler function

---
+++ handler.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
+
+func worker(jobs <-chan url.URL, results chan<- string) {
+	for j := range jobs {
+		resp, err := http.Get(j.String())
+		if err != nil {
+			results <- ""
+			continue
+		}
+
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			results <- ""
+			continue
+		}
+
+		results <- string(body)
+	}
+}
+
+//RSSMergeHandler configures borked handlers with timeouts and concurrency settings
+func RSSMergeHandler(w http.ResponseWriter, r *http.Request) {
+	sourceURLs := r.URL.Query()["source"]
+
+	if sourceURLs == nil || len(sourceURLs) == 0 {
+		http.Error(w, "missing feed source", http.StatusBadRequest)
+		return
+	}
+
+	sourceURL, err := url.Parse(sourceURLs[0])
+	if err != nil {
+		http.Error(w, "invalid source URL", http.StatusBadRequest)
+		return
+	}
+
+	feedURLs, err := FetchList(sourceURL)
+	if err != nil {
+		http.Error(w, "failed to fetch source list", http.StatusBadRequest)
+		return
+	}
+
+	feedJobs := make(chan url.URL, len(feedURLs))
+	feedResults := make(chan string, len(feedURLs))
+
+	for i := 1; i <= 10; i++ {
+		go worker(feedJobs, feedResults)
+	}
+
+	for _, v := range feedURLs {
+		feedJobs <- v
+	}
+	close(feedJobs)
+
+	var rawFeeds []string
+	for i := 1; i <= len(feedURLs); i++ {
+		rawFeeds = append(rawFeeds, <-feedResults)
+	}
+
+	mergedFeed := Merge(rawFeeds)
+
+	rss, err := mergedFeed.ToRss()
+	if err != nil {
+		http.Error(w, "failed to build merged feed", http.StatusBadRequest)
+		return
+	}
+
+	w.Write([]byte(rss))
+}
---
+++ handler_test.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/mmcdole/gofeed"
+)
+
+func TestScanEndpoint(t *testing.T) {
+	feedServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/feed_ars" {
+			pageContent, _ := ioutil.ReadFile("fixtures/feed_ars")
+			fmt.Fprintln(w, string(pageContent))
+		} else if r.URL.Path == "/feed_guardian" {
+			pageContent, _ := ioutil.ReadFile("fixtures/feed_guardian")
+			fmt.Fprintln(w, string(pageContent))
+		}
+	}))
+
+	gist := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/gist_path" {
+			pageContent := feedServer.URL + "/feed_ars\n" +
+				feedServer.URL + "/feed_guardian\n"
+
+			fmt.Fprintln(w, pageContent)
+		}
+	}))
+
+	req, err := http.NewRequest("GET", "/?source="+gist.URL+"/gist_path", nil)
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(RSSMergeHandler)
+
+	handler.ServeHTTP(rr, req)
+
+	body := rr.Body.String()
+
+	fp := gofeed.NewParser()
+	feed, _ := fp.ParseString(body)
+
+	if fmt.Sprintf("%v", feed.Items[0].Title) != "Canon Bill Hall obituary" {
+		t.Errorf("got %v want %v", feed.Items[0].Title, "Canon Bill Hall obituary")
+	}
+	if fmt.Sprintf("%v", feed.Items[0].Link) != "https://www.theguardian.com/world/2017/aug/27/canon-bill-hall-obituary" {
+		t.Errorf("got %v want %v", feed.Items[0].Link, "https://www.theguardian.com/world/2017/aug/27/canon-bill-hall-obituary")
+	}
+	if fmt.Sprintf("%v", feed.Items[0].PublishedParsed.String()) != "2017-08-27 17:15:33 +0000 UTC" {
+		t.Errorf("got %v want %v", feed.Items[0].PublishedParsed.String(), "2017-08-27 17:15:33 +0000 UTC")
+	}
+
+	if fmt.Sprintf("%v", feed.Items[len(feed.Items)-1].Title) != "Drug dens v safe houses: the fight over Ciudad Juárez's abandoned houses" {
+		t.Errorf("got %v want %v", feed.Items[len(feed.Items)-1].Title, "Drug dens v safe houses: the fight over Ciudad Juárez's abandoned houses")
+	}
+	if fmt.Sprintf("%v", feed.Items[len(feed.Items)-1].PublishedParsed) != "2017-08-11 09:00:54 +0000 UTC" {
+		t.Errorf("got %v want %v", feed.Items[len(feed.Items)-1].PublishedParsed.String(), "2017-08-11 09:00:54 +0000 UTC")
+	}
+}
